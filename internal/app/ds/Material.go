36ccf3e1a948db5afac60449e80f071f1b713e22
findwannawhy <findwannawhy@gmail.com>
2025-09-21 12:36:23 +0300

    fix: made mol_vol nullable as in task

--- internal/app/ds/Material.go
+++ internal/app/ds/Material.go
@@ -11,8 +11,8 @@ type Material struct {
 	Description               *string     `gorm:"type:text"                 json:"description"`
 	Deleted                    bool       `gorm:"not null;default:false"    json:"deleted"`
 	ImageURL                  *string     `gorm:"size:255"                  json:"image_url"`
-	RelativeMolecularMass      float64    `gorm:"not null"                  json:"relative_molecular_mass"`
-	StoichiometricCoefficient  float64    `gorm:"not null"                  json:"stoichiometric_coefficient"`
+	RelativeMolecularMass      float64    `gorm:"type:double precision;not null"                  json:"relative_molecular_mass"`
+	StoichiometricCoefficient  float64    `gorm:"type:double precision;not null"                  json:"stoichiometric_coefficient"`
 	
 	CreatedAt                  time.Time  `gorm:"<-:create;not null"        json:"created_at"`
 	UpdatedAt                  time.Time  `gorm:"not null"                  json:"updated_at"`
